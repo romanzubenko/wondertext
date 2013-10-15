de21cfa4a951c043c2cccf44f0790614fdc0f923
Roman Zubenko <romanz@bu.edu>
2013-10-15 17:48:32 -0400

    for use on website

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-	app.set('port', process.env.PORT || 8080);
+	app.set('port', process.env.PORT || 3002);
 	app.use(express.bodyParser());
 	app.use(express.static(path.join(__dirname, 'public')));
 });
